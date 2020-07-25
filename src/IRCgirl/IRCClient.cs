26eb64444dbe464a63480ab221f4c35b985cfcf2
Brosilio <brosilio@youshouldfear.me>
2020-07-25 08:29:42 -0400

    painstakingly added all IRC response codes as const ints in IRCReplyCodes.cs added some dumb boilerplate  to IRCServer.cs, IRCClient.cs, IRCCommand.cs, and IRCReader.cs added class IRCServerOptions.cs

---
+++ src/IRCgirl/IRCClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace libIRCgirl
+{
+	public class IRCClient
+	{
+		private TcpClient client;
+
+		public IRCClient(TcpClient client)
+		{
+			this.client = client;
+		}
+	}
+}
---
+++ src/IRCgirl/IRCServerOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace libIRCgirl
+{
+	public class IRCServerOptions
+	{
+
+	}
+}
