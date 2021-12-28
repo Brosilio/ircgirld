b19dc1cc74e996c760e9182d70dfac054d4b0ebb
Brosilio <15786675+Brosilio@users.noreply.github.com>
2021-12-28 00:45:54 -0500

    began rewrite

---
+++ src/ircgirld/ircgirld/IrcGirldRoutine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using libircgirl;
+using libircgirl.Server;
+
+namespace ircgirld
+{
+    internal class IrcGirldRoutine : IrcServerRoutine
+    {
+        public override Task OnConnect()
+        {
+            return base.OnConnect();
+        }
+
+        public override Task OnDisconnect()
+        {
+            return base.OnDisconnect();
+        }
+    }
+}
---
+++ src/ircgirld/ircgirld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using libircgirl;
+using libircgirl.Server;
+
+namespace ircgirld
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            IrcServer server = new IrcServer();
+            server.Start<IrcGirldRoutine>(IPAddress.Any, 6667);
+
+            server.WaitForExit();
+        }
+    }
+}
